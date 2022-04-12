09829b59c84f5efa01fa124bb72afe54c1a352f9
Iqbal Prince <iqbal.prince@northsouth.edu>
2022-04-12 20:23:50 +0600

    added images folder and edited some

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ $(document).ready(function(){
 
     // typing animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Actor", "Model", "theatre enthusiast "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Actor", "Model", "theatre enthusiast "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
