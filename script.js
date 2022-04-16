e75b1d95e4af9b54007644297f4abfa012f5c59f
Iqbal Prince <iqbal.prince@northsouth.edu>
2022-04-16 17:38:12 +0600

    added functionality in resume page

--- script.js
+++ script.js
@@ -23,4 +23,14 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
+    $('.list').click(function(){
+        const value = $(this).attr('data-filter');
+        if (value == 'All'){
+            $('.itemBox').show('1000');
+        }
+        else{
+            $('.itemBox').not('.'+value).hide('1000');
+            $('.itemBox').filter('.'+value).show('1000');
+        }
+    })
 });
